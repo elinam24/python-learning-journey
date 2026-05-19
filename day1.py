91928dfebd98c7d1f43e209d0ffbb9fed79a4638
Angel Elinam Wozborn <elinamwozborn@gmail.com>
2026-05-19 09:24:24 +0000

    Day 1 - Functions and Variables

---
+++ day1.py
@@ -0,0 +1,8 @@
+# Day 1 - Python Learning Journey
+# Aerospace Engineering learning to code
+
+name = "Angel Elinam Wozborn"
+goal = "Build AI systems for aircraft inspection"
+
+print("My name is", name)
+print("My goal is:", goal)
