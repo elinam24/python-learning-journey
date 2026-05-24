c9a5fd76024277b2d7a01f9b46178c6918b7d640
Angel Elinam Wozborn <elinamwozborn@gmail.com>
2026-05-24 21:37:10 +0000

    Day 3 - Letter Structure (List + Function + String + ...)

---
+++ day3.py
@@ -0,0 +1,30 @@
+# Combined lessons of dictionary, functions, strings, ...
+# Learnt how to create a letter structure
+
+def main():                                      # A function main
+    names = ["Denzel", "William", "Alexander"]   # list of names of receivers
+    for name in names:
+        print(letter(name, "Julius Annan"))      # two arguments for function letter
+
+def letter(receiver, sender):                    # Function letter with two areguments  
+    return f"""
+    +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+    # Letter write-up
+    Subject: Letter of Promotion                  
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Dear {receiver},
+
+    I am honored to send in my request of the 
+    office of the Managing Director.
+    With my qualification and level of commitment over
+    the years, I have proven to be the right fit for
+    this position.
+    Thank you so much for your time and consideration
+    on my request.
+    
+    Yours Sincerely,
+    {sender}
+    """
+
+main()                                         # function call
+
+#
